723051c67d05e293e4f845a0e5d22c0a5f5407a3
Mansi1716Varshney <92241290+Mansi1716Varshney@users.noreply.github.com>
2022-07-14 17:34:27 +0530

    Add files via upload

---
+++ interval.py
@@ -0,0 +1,50 @@
+#The number is prime or not
+"""first = int(input("Enter the first integer: ")) 
+last = int(input("\nEnter the last integer: ")) 
+
+print("\nPrime numbers between", first, "and", last, "are:")
+
+for num in range(first, last + 1):
+
+   if num > 1:
+       for i in range(2, num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)"""
+
+#Number is armstronge or not
+"""num = 9474
+order = len(str(num))
+sum = 0
+temp = num
+while temp > 0:
+   digit = temp % 10
+   sum += digit ** order
+   temp //= 10
+
+# display the result
+if num == sum:
+   print(num,"is an Armstrong number")
+else:
+   print(num,"is not an Armstrong number")"""
+
+#ACID property
+"""Atomicity
+Consistency
+Isolation
+Durability"""
+
+
+#What is machine learning?
+"""Machine learning is a branch of artificial intelligence (AI) and 
+computer science which focuses on the use of data and algorithms
+ to imitate the way that humans learn, gradually improving its accuracy"""
+
+
+#What is Inheritance?
+"""Inheritance is defined as a mechanism where the sub or child class inherits the properties and
+  characteristics of the super class or other derived classes.It also supports additional features of extracting properties
+  from the child class and using it into other derived classes."""
+
+#array and arraylist difference
